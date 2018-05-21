3df01fc45bf254fa5143349a5375c54f5329c88e
grantal <AlexV348@gmail.com>
2018-05-21 15:23:49 -0700

    add a component

--- src/App.js
+++ src/App.js
@@ -11,8 +11,21 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, boop <code>src/App.js</code> and save to reload.
         </p>
+        <BackpackItem name="PocketRocket 2" weight="2.6" desc="The next-generation MSR PocketRocket 2 backpacking stove takes everything good about the celebrated original and makes it even better. It's lighter weight and smaller, and fits a wider range of pots."></BackpackItem>
+      </div>
+    );
+  }
+}
+
+class BackpackItem extends Component {
+  render() {
+    return (
+      <div className="backpack-item">
+        <h5>{this.props.name}</h5>
+        <span><b>weight:</b> {this.props.weight} oz.</span><br/>
+        <span className="backpack-desc">{this.props.desc}</span>
       </div>
     );
   }
