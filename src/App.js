6828762fa52459046f62dc8cbb8bd698c7d22f8b
grantal <AlexV348@gmail.com>
2018-05-22 20:53:19 -0700

    can remove items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './images/logo.svg';
 import './App.css';
 import { ListGroup, ListGroupItem } from 'reactstrap';
 import gear from './gear.json'
@@ -8,34 +7,48 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      unselected: gear,
-      selected: [],
+      unselected: gear, //available gear
+      selected: [], // gear the user has selected
     };
   }
 
   // put item in the selected list
   selectItem(id){
-    const found = this.state.unselected.find(item => item["id"] === id);
+    let found = Object.assign({},this.state.unselected.find(item => item["id"] === id));
     const selected = this.state.selected.slice();
+    // make each item in selected have a unique id
+    found["id"] = selected.length.toString();
     this.setState({
       selected : selected.concat(found),
     });
   }
   // remove item from the selected list
   deselectItem(id){
+    const selected = this.state.selected.slice();
+    this.setState({
+      selected : selected.filter(item => item["id"] !== id),
+    });
+
   }
 
   render() {
     return (
-      <div className="App">
-        <ItemPool 
-          items={this.state.unselected}
-          itemClick={id => this.selectItem(id)}
-        ></ItemPool>
-        <ItemPool 
-          items={this.state.selected}
-          itemClick={id => this.deselectItem(id)}
-        ></ItemPool>
+      <div className="container-fluid">
+        <div className="row">
+          <ItemPool 
+            items={this.state.unselected}
+            itemClick={id => this.selectItem(id)}
+            className="col-sm p-0"
+          ></ItemPool>
+          <div className="col-sm p-0">
+            <WeightCounter weight={13}></WeightCounter>
+            <ItemPool 
+              items={this.state.selected}
+              itemClick={id => this.deselectItem(id)}
+              className="selected-items"
+            ></ItemPool>
+          </div>
+        </div>
       </div>
     );
   }
@@ -44,7 +57,7 @@ class App extends Component {
 class ItemPool extends Component {
   render() {
     return (
-      <div className="item-pool">
+      <div className={this.props.className}>
         <ListGroup>
           {
             this.props.items.map(item => {
@@ -63,27 +76,23 @@ class ItemPool extends Component {
 }
 
 class BackpackItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {hidden: false};
-    this.handleClick = this.handleClick.bind(this);
-  }
-  handleClick() {
-    this.setState(prevState => ({
-      hidden: !prevState.hidden
-    }));
-  }
-
   render() {
-    if (this.state.hidden) {
-      return (<div hidden></div>);
-    }
     return (
       <ListGroupItem className="backpack-item" onClick={this.props.onClick}>
         <h5>{this.props.name}</h5>
         <span><b>weight:</b> {this.props.weight} oz.</span><br/>
         <span className="backpack-desc">{this.props.desc}</span>
       </ListGroupItem>
+    );
+  }
+}
+
+class WeightCounter extends Component {
+  render() {
+    return (
+      <div className="weight-counter">
+        <span><b>Total Weight:</b> {this.props.weight} oz.</span>
+      </div>
     );
   }
 }
